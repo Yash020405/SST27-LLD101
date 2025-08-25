1c0d7ebad2e539fc0ae836ef4ddd0a1f4642e865
Yash020405 <yashaga0204@gmail.com>
2025-08-25 11:17:49 +0530

    Answer : Exercise 10

--- SOLID/ex10/src/Demo10.java
+++ SOLID/ex10/src/Demo10.java
@@ -1,5 +1,16 @@
 public class Demo10 {
     public static void main(String[] args) {
-        new ReportService().generate();
+        Logger consoleLogger = new ConsoleLogger();
+        ReportService service = new ReportService(consoleLogger);
+        service.generate();
+
+        // for testing
+        Logger mockLogger = new Logger() {
+            @Override
+            public void log(String msg) {
+                System.out.println("[MOCK] " + msg);
+            }
+        };
+        new ReportService(mockLogger).generate();
     }
 }
